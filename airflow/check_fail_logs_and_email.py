080aef475d608c1acfdcb241c3c287ba32a3887a
kimseoryeong <datadev2402@gmail.com>
2025-07-11 16:28:20 +0900

    send email 수정

--- airflow/check_fail_logs_and_email.py
+++ airflow/check_fail_logs_and_email.py
@@ -56,7 +56,7 @@ with DAG(
     # 이메일 전송 태스크
     send_email_task = EmailOperator(
         task_id='send_email_alert',
-        to='datadev2402@gmail.com',
+        to='srkim@zenithcloud.com',
         subject='{{ data_interval_end.in_timezone("Asia/Seoul") | ds }} ETL 실패 로그 알림',
         html_content="""
         {{ ti.xcom_pull(task_ids='check_fail_db_logs') }}
