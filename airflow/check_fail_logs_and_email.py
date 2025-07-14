72aa98fee31782d25803606ad7251e2cbb1f5611
kimseoryeong <datadev2402@gmail.com>
2025-07-14 11:10:15 +0900

    html_content 수정

--- airflow/check_fail_logs_and_email.py
+++ airflow/check_fail_logs_and_email.py
@@ -62,8 +62,10 @@ with DAG(
         task_id="send_email_alert",
         conn_id="smtp_default",
         to="srkim@zenithcloud.com",
-        subject='{{ dag_run.logical_date.in_timezone("Asia/Seoul") | ds }} ETL 실패 로그 알림',
-        html_content='{{ ti.xcom_pull(task_ids="check_fail_db_logs") }}',
+        subject='{{data_interval_end.in_timezone("Asia/Seoul") | ds}}  ETL 실패 로그 알림',
+        html_content='{{data_interval_end.in_timezone("Asia/Seoul") | ds}} 처리결과는 <br> \ '
+                     '{{ ti.xcom_pull(task_ids="check_fail_db_logs") }}<br>'
+
     )
 
     check_fail_logs() >> send_email_alert
